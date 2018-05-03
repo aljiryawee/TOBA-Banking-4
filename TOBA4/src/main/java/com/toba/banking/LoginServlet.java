11f3d774c12d5e0d8b71ca78cb11593a921c38d5
aljiryawee <37884703+aljiryawee@users.noreply.github.com>
2018-05-03 14:40:02 -0700

    Update LoginServlet.java

--- TOBA4/src/main/java/com/toba/banking/LoginServlet.java
+++ TOBA4/src/main/java/com/toba/banking/LoginServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-//@WebServlet(urlPatterns = "/loginServlet")
 public class LoginServlet extends HttpServlet {
 
 	Logger logger = Logger.getLogger(LoginServlet.class.getName());
@@ -19,29 +18,10 @@ public class LoginServlet extends HttpServlet {
 			throws IOException, ServletException {
 
 		request.getRequestDispatcher("/WEB-INF/views/login.html").forward(request, response);
-		// throw new ServletException("Hello World");
+		
 	}
 
-	/*
-	 * @Override protected void doPost(HttpServletRequest request,
-	 * HttpServletResponse response) throws ServletException, IOException {
-	 * 
-	 * String username = request.getParameter("username"); String password =
-	 * request.getParameter("password");
-	 * 
-	 * User user = new User(); user.setUsername(username);
-	 * user.setPassword(password); user.setFirstName("J.");
-	 * user.setLastName("Smith");
-	 * 
-	 * HttpSession httpSession = request.getSession();
-	 * httpSession.setAttribute("user", user);
-	 * 
-	 * if(username.equals("jsmith@toba.com") && password.equals("letmein"))
-	 * request.getRequestDispatcher("/WEB-INF/views/account_activity.jsp").
-	 * forward(request, response); else
-	 * request.getRequestDispatcher("/WEB-INF/views/login_failure.html").forward
-	 * (request, response); }
-	 */
+
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
